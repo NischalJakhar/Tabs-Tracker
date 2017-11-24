92296fab59a6e5b21ffc8edf7703f3cb3237107d
NischalJakhar <nischaljakhar@gmail.com>
2017-11-24 09:36:26 +0530

    Setup of expressjs server

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+console.log("Start");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status',(req,res)=>{
+    res.send({
+    message:'hello'
+});
+});
+
+
+app.listen(8082,function(){
+    console.log("Server started on http://localhost:8082");
+});
+
+
+
+
+
