c6c9a9a6f036eb530ea0ab6d87c6a107882d3c01
prateek951 <prince.cenation@gmail.com>
2017-12-28 15:20:32 +0530

    added the registration template

---
+++ client/src/components/Register.vue
@@ -0,0 +1,58 @@
+<template>
+	<div>
+		<h1>Register</h1>
+		<input type="email" placeholder="Enter the email" name="email" v-model="email">
+		<br><br>
+		<input type="password" placeholder="Enter the password" name="password" v-model="password">
+		<br><br>
+		<button type="submit" @click="register">Register</button>
+	</div>
+</template>
+
+<script>
+// Everything inside the script tag is your controller
+// I can bind my html to the controller
+
+/* eslint-disable*/ 
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+		  email: '',
+		  password: ''
+  		}
+	},
+	methods: {
+		async register () {
+			// console.log('Register button was clicked',this.email, this.password)
+			const response = await AuthenticationService.register({
+				email: this.email,
+				password: this.password
+			})
+			console.log(response.data)
+		}
+	}
+
+	// watch: {
+	// 	// Watching the changes in the email field
+	// 	email (value) {
+	// 		console.log('Email has changed', value)
+	// 	}
+	// },
+	// Whenever the vue component is mounted on the page, I want to run a command
+	// mounted () {
+	// 	// Data model changes the actual view
+	// 	setTimeout(()=> {
+	// 		this.email = 'Hello World'
+	// 		this.password = 'rockstar'
+	// },2000)
+	// }
+
+}
+	
+</script>
+
+
+<style scoped>
+	
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -9,5 +9,6 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => res.send({message: 'Hello World'}))
+// For testing pass a payload for email using postman
+app.post('/register', (req, res) => res.send({message: `Hello ${req.body.email}!!!Your User was registered`}))
 app.listen(process.env.PORT || 1777)
