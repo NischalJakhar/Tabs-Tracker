309c2b252d34507092aee8c00ec361fa4bfc253c
prateek951 <prince.cenation@gmail.com>
2017-12-28 13:19:16 +0530

    setup of the expressjs server

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,13 @@
-console.log("Start");
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+console.log('Hello World')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.get('/status',(req,res)=> res.send({message:'hello'}));
-
-const port = 1337 || process.env.PORT;
-app.listen(port,() => console.log(`Server started on http://localhost:${port} `));
-
-
-
-
-
+app.get('/status', (req, res) => res.send({message: 'Hello World'}))
+app.listen(process.env.PORT || 1777)
